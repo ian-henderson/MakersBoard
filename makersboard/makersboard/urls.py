2bf175d7cf13972a1b50480c2a766e55f17ee359
Ian Henderson <iancurtish@gmail.com>
2016-04-26 18:51:49 -0500

    Little button on the navbar & set / to /posts

--- makersboard/makersboard/urls.py
+++ makersboard/makersboard/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', include("posts.urls", namespace="posts")),
     url(r'^posts/', include("posts.urls", namespace='posts')),
     url(r'^users/', include("users.urls", namespace='users')),
 ]
